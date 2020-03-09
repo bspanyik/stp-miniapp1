cce41b9ec80c2c060df671cb837b73c220fd7ddf
bspanyik <spanyik.balazs@gmail.com>
2020-03-09 15:35:04 +0100

    Modify migration
    - change password column type to char(40) (was: binary(20))
    - add unique index on rfid column
    - add new data table with foreign key constraint to users' rfid

--- src/Migrations/Version20200309130952.php
+++ src/Migrations/Version20200309130952.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200304140714 extends AbstractMigration
+final class Version20200309130952 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -28,16 +28,32 @@ final class Version20200304140714 extends AbstractMigration
                 active   TINYINT(1)   NOT NULL, 
                 name     VARCHAR(255) NOT NULL, 
                 roles    JSON         NOT NULL, 
-                password BINARY(20)   NOT NULL,
+                password CHAR(40)   NOT NULL,
                 rfid     VARCHAR(255) NOT NULL, 
                 company  VARCHAR(255) NOT NULL, 
                 address  VARCHAR(255) NOT NULL, 
                 comment  VARCHAR(255) NOT NULL, 
                 UNIQUE INDEX UNIQ_1483A5E95E237E06 (name), 
+                UNIQUE INDEX UNIQ_1483A5E952C87536 (rfid),
                 PRIMARY KEY(id)
             ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
 
         ');
+
+        $this->addSql('
+
+            CREATE TABLE data (
+                id INT AUTO_INCREMENT NOT NULL, 
+                rfid VARCHAR(255) NOT NULL, 
+                time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+                direction VARCHAR(255) NOT NULL, 
+                INDEX IDX_ADF3F36352C87536 (rfid), 
+                PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+
+        );
+
+        $this->addSql('ALTER TABLE data ADD CONSTRAINT FK_ADF3F36352C87536 FOREIGN KEY (rfid) REFERENCES users (rfid)');
     }
 
     public function down(Schema $schema) : void
